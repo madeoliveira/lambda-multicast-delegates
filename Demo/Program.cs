76e57d4a6c9da42d2b5ef488d4492cfd33b79c19
Marcio Oliveira <marcio.alves78@hotmail.com>
2021-02-15 16:38:48 -0300

    Delegates whor keep a reference for more than one method.

---
+++ Demo/Program.cs
@@ -0,0 +1,24 @@
+﻿using Course.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    delegate void BinaryNumericOperation(double n1, double n2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 234;
+            double b = 432;
+            BinaryNumericOperation op = CalculationService.ShowMax;
+            op += CalculationService.ShowSum;
+
+            op.Invoke(a, b);
+
+        }
+    }
+}
